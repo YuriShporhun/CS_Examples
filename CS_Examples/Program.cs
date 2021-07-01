1c407596ecbffd67ea98cfae92784ec240a70741
Юрий <YuriPCLive@live.ru>
2021-07-01 19:36:30 +0300

    Добавьте файлы проекта.

---
+++ CS_Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
